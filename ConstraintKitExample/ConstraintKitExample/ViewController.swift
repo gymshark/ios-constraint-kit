737f3e8b5b6de6aa192a832faa6ac5e60da8fa85
Russell Warwick <russell.warwick@gymshark.com>
2022-06-06 23:06:56 +0100

    Adds ios9 support

--- ConstraintKitExample/ConstraintKitExample/ViewController.swift
+++ ConstraintKitExample/ConstraintKitExample/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import ConstraintKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "This is a test"
+        label.backgroundColor = .red
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(label)
+        label.withSize(.init(w: 100, h: 120))
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
 }
